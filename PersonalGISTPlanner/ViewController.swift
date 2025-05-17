de3c94537e6e67b8699c9c8c6a7946994ae3bff3
Dinda Ayu Syafitri <85775024+dinda-ayu-syafitri@users.noreply.github.com>
2025-05-17 17:02:27 +0700

    Initial Commit

---
+++ PersonalGISTPlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalGISTPlanner
+//
+//  Created by Dinda Ayu Syafitri on 17/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
