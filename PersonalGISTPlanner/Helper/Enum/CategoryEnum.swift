c8d2feb69788dd5abb4a3be42f2e353a08cbabf0
Dinda Ayu Syafitri <85775024+dinda-ayu-syafitri@users.noreply.github.com>
2025-05-26 11:25:28 +0700

    feat: Add plan category enum

---
+++ PersonalGISTPlanner/Helper/Enum/CategoryEnum.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryEnum.swift
+//  PersonalGISTPlanner
+//
+//  Created by Dinda Ayu Syafitri on 26/05/25.
+//
+
+enum PlanCategory {
+    case goal
+    case idea
+    case task
+    case step
+    case none
+}
