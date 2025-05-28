86393b68062f552494889d955979c04c8bed972f
Dinda Ayu Syafitri <85775024+dinda-ayu-syafitri@users.noreply.github.com>
2025-05-28 10:37:42 +0700

    feat: Due date input integration

--- PersonalGISTPlanner/Presentation/Input/InputViewController.swift
+++ PersonalGISTPlanner/Presentation/Input/InputViewController.swift
@@ -14,25 +14,38 @@ class InputViewController: UIViewController {
     @IBOutlet var categorySelection: UIView!
     @IBOutlet var categoryLabel: UILabel!
     @IBOutlet var dueDateView: UIView!
+    @IBOutlet var dueDatePicker: UIDatePicker!
 
     let viewModel = InputViewModel()
     var selectedCategory: PlanCategory = .none
+    var selectedDueDate: Date?
 
     // MARK: - LIFECYCLE
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
         setupGestureRecognizers()
     }
 
     // MARK: - SETUP
 
+    private func setupView() {
+        dueDatePicker.isHidden = true
+    }
+
     private func setupGestureRecognizers() {
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(pushToCategoryView))
-        categorySelection.addGestureRecognizer(tapGesture)
+        let tapCategorySelectionGesture = UITapGestureRecognizer(target: self, action: #selector(pushToCategoryView))
+        categorySelection.addGestureRecognizer(tapCategorySelectionGesture)
         categorySelection.isUserInteractionEnabled = true
 
+        let tapDueDateViewGesture = UITapGestureRecognizer(target: self, action: #selector(toggleDueDatePicker))
+        dueDateView.addGestureRecognizer(tapDueDateViewGesture)
+        dueDateView.isUserInteractionEnabled = true
+
         submitButton.addTarget(self, action: #selector(submit), for: .touchUpInside)
+
+        dueDatePicker.addTarget(self, action: #selector(datePickerValueChanged(_:)), for: .valueChanged)
     }
 
     // MARK: - ACTION
@@ -42,12 +55,13 @@ class InputViewController: UIViewController {
         let plan = Plan()
         plan.title = titleInput.text ?? ""
         plan.desc = descInput.text ?? ""
-        plan.dueDate = Date().addingTimeInterval(60 * 60 * 24 * 2)
+        plan.dueDate = selectedDueDate ?? Date()
         plan.category = selectedCategory
 
         print("title: \(plan.title)")
         print("desc: \(plan.desc)")
         print("category: \(plan.category)")
+        print("Due Date: \(plan.dueDate)")
 
         viewModel.savePlan(plan)
     }
@@ -62,5 +76,15 @@ class InputViewController: UIViewController {
         }
 
         navigationController?.pushViewController(categoryVC, animated: true)
+    }
+
+    // DATE PICKER
+
+    @objc func toggleDueDatePicker() {
+        dueDatePicker.isHidden.toggle()
+    }
+
+    @objc func datePickerValueChanged(_ sender: UIDatePicker) {
+        selectedDueDate = sender.date
     }
 }
